51008c8c130a714720ba58b51a4a515e7639eada
Lateralus- <elena.skorchenko.y@gmail.com>
2022-01-04 14:30:30 +0200

    Improve Security Using Validation

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,45 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+    [Required]
         public int Id { get; set; }
-        [Display(Name = "First name")]
+
+    [Required]
+    [DataType(DataType.Text)]
+    [StringLength(maximumLength: 100, MinimumLength = 2)]
+    [Display(Name = "First name")]
         public string FirstName { get; set; }
-        [Display(Name = "Last name")]
+
+    [Required]
+    [DataType(DataType.Text)]
+    [StringLength(maximumLength: 100, MinimumLength = 2)]
+    [Display(Name = "Last name")]
         public string LastName { get; set; }
-        public string Description { get; set; }
-        [Display(Name = "Email Address")]
+
+    [Required]
+    [DataType(DataType.MultilineText)]
+    [StringLength(maximumLength: 500, MinimumLength = 10)]
+    public string Description { get; set; }
+
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
-        [Display(Name = "Phone Number")]
+
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
         public bool IsStaff { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+      var results = new List<ValidationResult>();
+      if (EmailAddress.Contains("TechnologyLiveConference.com", StringComparison.InvariantCultureIgnoreCase))
+        results.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+
+      return results;
     }
+  }
 }
